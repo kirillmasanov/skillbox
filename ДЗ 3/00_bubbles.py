735776088814c1542dfa39445debaae0b16dad15
Kirill Masanov <kirill_masanov@mail.ru>
2020-01-11 00:11:29 +0300

    done

--- ДЗ 3/00_bubbles.py
+++ ДЗ 3/00_bubbles.py
@@ -9,16 +9,29 @@ sd.resolution = (1200, 600)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
+def bubble(point, step):
+    radius = 50
+    for i in range(3):
+        radius += step
+        sd.circle(point, radius=radius, width=2)
+
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
 
+
 # Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
+# for y in range(100, 401, 100):
+#     for x in range(100, 1001, 100):
+#         point = sd.get_point(x, y)
+#         bubble(point=point, step=10)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
-
+for i in range(100):
+    point = sd.random_point()
+    bubble(point=point, step=10)
 sd.pause()
 
 
