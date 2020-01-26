97840f0eceb93d8e922d584f293469ef161cbd67
Kirill Masanov <kirill_masanov@mail.ru>
2020-01-26 20:18:05 +0300

    вариант 2 - с классами

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -22,30 +22,48 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 
-def scan_book(book_name):
-    with open(book_name, 'r', encoding='cp1251') as file:
-        my_dict = {}
+class CharStat:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def dict_word(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            my_dict = {}
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char not in my_dict:
+                            my_dict[char] = 1
+                        else:
+                            my_dict[char] += 1
+        return my_dict
+
+    def sort_key_alpha_up(self):
+        sort_key = sorted(self.dict_word())
+        return sort_key
+
+    def print_dict(self, sort_key, my_dict):
+        sum_rate = 0
         print(f'+{"+":-^20}+')
         print(f'|{"буква": ^9}|{"частота": ^10}|')
         print(f'+{"+":-^20}+')
-        for line in file:
-            for char in line:
-                if char.isalpha():
-                    if char not in my_dict:
-                        my_dict[char] = 1
-                    else:
-                        my_dict[char] += 1
-    sum_rate = 0
-    sort_key = sorted(my_dict)
-    for key in sort_key:
-        print(f'|{key: ^9}|{my_dict[key]: ^10}|')
-        sum_rate += my_dict[key]
-    print(f'+{"+":-^20}+')
-    print(f'|{"итого": ^9}|{sum_rate: ^10}|')
-    print(f'+{"+":-^20}+')
+        for key in sort_key:
+            print(f'|{key: ^9}|{my_dict[key]: ^10}|')
+            sum_rate += my_dict[key]
+        print(f'+{"+":-^20}+')
+        print(f'|{"итого": ^9}|{sum_rate: ^10}|')
+        print(f'+{"+":-^20}+')
 
 
-scan_book('voyna-i-mir.txt')
+book1 = CharStat(file_name='voyna-i-mir.txt')
+my_dict = book1.dict_word()
+
+sort_key = book1.sort_key_alpha_up()
+book1.print_dict(sort_key=sort_key, my_dict=my_dict)
+
+
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
