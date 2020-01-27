dbdc40d8a1108e5d37d1aa1f2516f7a318adf8fe
Kirill Masanov <kirill_masanov@mail.ru>
2020-01-27 20:45:05 +0300

    лог парсер на классах

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,7 +19,62 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+
+class LogParser_min:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.dict_min = {}
+
+    def calc_file(self):
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                if line[-4] == 'N':
+                    if line[:17] + ']' not in self.dict_min:
+                        self.dict_min[line[:17] + ']'] = 1
+                    else:
+                        self.dict_min[line[:17] + ']'] += 1
+
+    def print_dict_min(self):
+        for key, value in self.dict_min.items():
+            print(f'{key} {value}')
+
+
+class LogParserHour(LogParser_min):
+    def calc_file(self):
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                if line[-4] == 'N':
+                    if line[:14] + ']' not in self.dict_min:
+                        self.dict_min[line[:14] + ']'] = 1
+                    else:
+                        self.dict_min[line[:14] + ']'] += 1
+
+
+class LogParserMonth(LogParser_min):
+    def calc_file(self):
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                if line[-4] == 'N':
+                    if line[:8] + ']' not in self.dict_min:
+                        self.dict_min[line[:8] + ']'] = 1
+                    else:
+                        self.dict_min[line[:8] + ']'] += 1
+
+
+class LogParserYear(LogParser_min):
+    def calc_file(self):
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                if line[-4] == 'N':
+                    if line[:5] + ']' not in self.dict_min:
+                        self.dict_min[line[:5] + ']'] = 1
+                    else:
+                        self.dict_min[line[:5] + ']'] += 1
+
+
+my_parser = LogParserYear(file_name='events.txt')
+my_parser.calc_file()
+my_parser.print_dict_min()
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
