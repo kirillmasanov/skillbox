91770035087a50926a3b7fe28f650c45bbd2ca88
kirill_masanov <kirill_masanov@mail.ru>
2020-02-16 02:47:06 +0300

    volatility part 1

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -74,3 +74,79 @@
 #         <обработка данных>
 
 # TODO написать код в однопоточном/однопроцессорном стиле
+import os
+from collections import defaultdict
+import time
+
+
+class Volatility:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.volatility = 0
+        self.sum_price = 0
+        self.average_price = 0
+        self.min_price = 99999999
+        self.max_price = 0
+        self.count = 0
+
+    def run(self):
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            file.readline()
+            for line in file:
+                price = float(line.split(',')[2])
+                self.sum_price += price
+                self.count += 1
+                if self.min_price > price:
+                    self.min_price = price
+                if self.max_price < price:
+                    self.max_price = price
+            self.average_price = self.sum_price / self.count
+            self.volatility = ((self.max_price - self.min_price) / self.average_price) * 100
+
+
+def make_file_list(scan_folder):
+    file_list = []
+    for dir_path, dir_name, file_name in os.walk(scan_folder):
+        for file in file_name:
+            file_list.append(os.path.join(dir_path, file))
+    return file_list
+
+
+def time_track(func, *args, **kwargs):
+    started_at = time.time()
+
+    result = func(*args, **kwargs)
+
+    ended_at = time.time()
+    elapsed = round(ended_at - started_at, 4)
+    print(f'Функция работала {elapsed} секунд(ы)')
+    return result
+
+
+@time_track
+def main():
+    file_list = make_file_list('trades')
+    volatilities = [Volatility(file_name=file) for file in file_list]
+    volatility_dict = defaultdict()
+    null_dict = defaultdict()
+    for ff in volatilities:
+        ff.run()
+        f_name = os.path.basename(ff.file_name)[:-4]
+        if ff.volatility == 0:
+            null_dict[f_name] = round(ff.volatility, 2)
+        else:
+            volatility_dict[f_name] = round(ff.volatility, 2)
+    sort_dict = sorted(volatility_dict.items(), key=lambda kv: kv[1], reverse=True)
+    print(f'Максимальная волатильность:')
+    for i in range(3):
+        print(f'{sort_dict[i][0]} - {sort_dict[i][1]} %')
+    print(f'Минимальная волатильность:')
+    for i in range(2, -1, -1):
+        print(f'{sort_dict[-i - 1][0]} - {sort_dict[-i - 1][1]} %')
+    print(f'Нулевая волатильность:')
+    print(', '.join(list(null_dict.keys())))
+
+
+if __name__ == '__main__':
+    main()
